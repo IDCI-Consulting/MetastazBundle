dfc2fe7583dac090a2abc2a5cb6f993e062c0e8b
Thibaut Freydier <thibaut.f@allopneus.com>
2011-08-11 17:54:51 +0200

    Ajout MetastazODMMetastazStore

---
+++ lib/Metastaz/Stores/DoctrineODMMetastazStore.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace Metastaz\Stores;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Metastaz\Bundle\MetastazBundle\MetastazBundle;
+use Metastaz\Bundle\MetastazBundle\Document\Metastaz;
+use Metastaz\MetastazStore;
+
+/**
+ * DoctrineMetastazStore is a concrete provider to store Metastazs throw 
+ * Doctrine ODM.
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @licence: LGPL
+ */
+class DoctrineODMMetastazStore extends MetastazStore
+{
+    protected $dm = null;
+
+    /**
+     * Get the Entity Manager
+     *
+     * 
+     */
+    protected function getDocumentManager()
+    {
+        if(!$this->em)
+        {
+            $params = $this->getMetastazContainer()->getParameter('metastaz.store_parameters');
+            
+            $this->em = MetastazBundle::getContainer()
+                ->get('doctrine.odm.mongodb.document_manager')               
+            ;
+        }
+       
+        return $this->em;
+    }
+
+    /**
+     * @see Hevea\Bundle\MetastazBundle\Stores\MetastazStore
+     * @throw NotFoundHttpException
+     */
+    public function get($dimension, $namespace, $key, $culture = null)
+    {
+        $dm = $this->getDocumentManager();
+        
+        $entity = $dm->getRepository('MetastazBundle:Metastaz')->findOneBy(
+            array(
+                'meta_dimension' => $dimension,
+                'meta_namespace' => $namespace,
+                'meta_key' => $key
+            )
+        );
+
+        if (!$entity) {
+            throw new NotFoundHttpException(
+                sprintf('Unable to find Metastaz entity with the following parameter: %s %s %s.',
+                    $dimension,
+                    $namespace,
+                    $key
+                )
+            );
+        }
+
+        //TODO: Return data in function of the culture parameter   
+        return $entity->getMetaValue();
+    }
+
+    /**
+     * @see Hevea\Bundle\MetastazBundle\Stores\MetastazStore
+     */
+    public function put($dimension, $namespace, $key, $value, $culture = null)
+    {               
+        $dm = $this->getDocumentManager();
+        $entity = $dm->getRepository('MetastazBundle:Metastaz')->findOneBy(
+            array(
+                'meta_dimension' => $dimension,
+                'meta_namespace' => $namespace,
+                'meta_key' => $key
+            )
+        );              
+
+        if (!$entity) {
+            $entity = new Metastaz();
+            $entity->setMetaDimension($dimension);
+            $entity->setMetaNamespace($namespace);
+            $entity->setMetaKey($key);
+        }
+
+        //TODO: Save data in function of the culture parameter
+        $entity->setMetaValue($value);
+        $dm->persist($entity);        
+        $dm->flush();
+    }
+
+    /**
+     * @see Hevea\Bundle\MetastazBundle\Stores\MetastazStore
+     */
+    public function getAll($dimension, $namespace)
+    {
+        $dm = $this->getDocumentManager();
+        $entities = $dm->getRepository('MetastazBundle:Metastaz')->findBy(
+            array(
+                'meta_dimension' => $dimension,
+                'meta_namespace' => $namespace
+            )
+        );
+
+        $ret = array();
+        foreach($entities as $entity) {
+            $ret[$entity->getMetaKey()] = $entity->getMetaValue();
+        }
+        return $ret;
+    }
+
+    /**
+     * @see Hevea\Bundle\MetastazBundle\Stores\MetastazStore
+     * @throw NotFoundHttpException
+     */
+    public function delete($dimension, $namespace, $key)
+    {
+        $dm = $this->getDocumentManager();
+        $entity = $dm->getRepository('MetastazBundle:Metastaz')->findOneBy(
+            array(
+                'meta_dimension' => $dimension,
+                'meta_namespace' => $namespace,
+                'meta_key' => $key
+            )
+        );
+
+        if (!$entity) {
+            throw new NotFoundHttpException(
+                sprintf('Unable to find Metastaz entity with the following parameter: %s %s %s.',
+                    $dimension,
+                    $namespace,
+                    $key
+                )
+            );
+        }
+
+        $dm->remove($entity);
+        $dm->flush();
+    }
+
+    /**
+     * @see Hevea\Bundle\MetastazBundle\Stores\MetastazStore
+     * @throw NotFoundHttpException
+     */
+    public function deleteAll($dimension)
+    {
+        $dm = $this->getDocumentManager();
+        $entities = $dm->getRepository('MetastazBundle:Metastaz')->findBy(
+            array('meta_dimension' => $dimension)
+        );
+
+        foreach($entities as $entity) {
+            $dm->remove($entity);
+        }
+        $dm->flush();
+    }
+}
---
+++ src/Metastaz/Bundle/MetastazBundle/Document/Metastaz.php
@@ -0,0 +1,142 @@
+<?php
+namespace Metastaz\Bundle\MetastazBundle\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * Metastaz is a concrete store for metadata.
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @author:  Michel ROTTA <michel.r@allopneus.com>
+ * @licence: LGPL
+ * @MongoDB\Document
+ * 
+ */
+class Metastaz
+{
+    /**
+     * @MongoDB\Id 
+     */
+    protected $id;
+
+    /**
+     * Dimension
+     *
+     * @MongoDB\String
+     */
+    protected $meta_dimension;
+
+    /**
+     * Namespace
+     *
+     * @MongoDB\String
+     */
+    protected $meta_namespace;
+
+    /**
+     * Key
+     *
+     * @MongoDB\String
+     */
+    protected $meta_key;
+
+    /**
+     * Value
+     *
+     * @MongoDB\String
+     */
+    protected $meta_value;
+
+    
+    
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set meta_dimension
+     *
+     * @param string $metaDimension
+     */
+    public function setMetaDimension($metaDimension)
+    {
+        $this->meta_dimension = $metaDimension;
+    }
+
+    /**
+     * Get meta_dimension
+     *
+     * @return string $metaDimension
+     */
+    public function getMetaDimension()
+    {
+        return $this->meta_dimension;
+    }
+
+    /**
+     * Set meta_namespace
+     *
+     * @param string $metaNamespace
+     */
+    public function setMetaNamespace($metaNamespace)
+    {
+        $this->meta_namespace = $metaNamespace;
+    }
+
+    /**
+     * Get meta_namespace
+     *
+     * @return string $metaNamespace
+     */
+    public function getMetaNamespace()
+    {
+        return $this->meta_namespace;
+    }
+
+    /**
+     * Set meta_key
+     *
+     * @param string $metaKey
+     */
+    public function setMetaKey($metaKey)
+    {
+        $this->meta_key = $metaKey;
+    }
+
+    /**
+     * Get meta_key
+     *
+     * @return string $metaKey
+     */
+    public function getMetaKey()
+    {
+        return $this->meta_key;
+    }
+
+    /**
+     * Set meta_value
+     *
+     * @param string $metaValue
+     */
+    public function setMetaValue($metaValue)
+    {
+        $this->meta_value = $metaValue;
+    }
+
+    /**
+     * Get meta_value
+     *
+     * @return string $metaValue
+     */
+    public function getMetaValue()
+    {
+        return $this->meta_value;
+    }
+}
