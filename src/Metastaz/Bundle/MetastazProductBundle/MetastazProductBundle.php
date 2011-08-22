50efaa686a75ef29990a3b4be8421f4ecaa750a0
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2011-08-22 10:02:02 +0200

    Start MetastazProductBundle

---
+++ src/Metastaz/Bundle/MetastazProductBundle/MetastazProductBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Metastaz\Bundle\MetastazProductBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * MetastazProductBundle.
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @licence: GPL
+ */
+class MetastazProductBundle extends Bundle
+{
+}
